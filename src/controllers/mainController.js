115bae0ac43b348797db3fe53422aca7a39d2381
WaelS87 <wfakhrji@gmail.com>
2022-08-14 18:32:12 -0300

    aregle la vista

---
+++ src/controllers/mainController.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		const inSale = products.filter(product=>product.category==='in-sale')
+		const visited = products.filter(product=>product.category==='visited')
+		return res.render('index',{
+			inSale,
+			visited
+		})
+	
+	},
+	search: (req, res) => {
+		// Do the magic
+	},
+};
+
+module.exports = controller;
